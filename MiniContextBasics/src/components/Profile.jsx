0a45c51aecf5df27db77b594146a984d4522083f
The-Parthib <rijupanja81@gmail.com>
2024-12-08 23:44:29 +0530

    ContextAPI

---
+++ MiniContextBasics/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Profile() {
+  const { user } = useContext(UserContext);
+
+  if (!user) return <h3>Please Login</h3>
+
+  return <div>Wlcome user {user.username}</div>
+}
+
+export default Profile;
